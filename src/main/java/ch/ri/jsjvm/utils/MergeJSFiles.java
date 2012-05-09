0d0a233696b24cbaf5aa76c72900d388411dc7cf
NatureFreshMeat <naturefreshmeet@github.com>
2012-05-09 12:36:21 +0200

    Added JSFileMerger

---
+++ src/main/java/ch/ri/jsjvm/utils/MergeJSFiles.java
@@ -0,0 +1,48 @@
+package ch.ri.jsjvm.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.TreeSet;
+
+public class MergeJSFiles
+{
+	
+	public static void main(String[] args) throws Exception
+	{
+		
+		File jsDir = new File("src/main/javascript");
+		File outputFile = new File("target/jsjvm.js");
+		
+		if (outputFile.exists())
+			outputFile.delete();
+		
+		PrintStream printer = new PrintStream(outputFile);
+		
+		TreeSet<File> set = new TreeSet<File>();
+		
+		for (File f: jsDir.listFiles())
+		{
+			if (f.isFile())
+				set.add(f);
+		}
+		
+		for (File f: set)
+		{
+			BufferedReader reader = new BufferedReader( new FileReader(f) );
+			
+			while (reader.ready())
+			{
+				String line = reader.readLine();
+				printer.println( line );
+				System.out.println( line );
+			}
+		}
+		
+		printer.flush();
+		printer.close();
+		
+	}
+
+}
